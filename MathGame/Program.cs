13948d0b7e9725aa237d82ac52ae10aad012a720
Aleksander Wons <aleksander.wons@gmail.com>
2024-09-27 19:31:45 +0200

    Move game loop into a separate class

--- MathGame/Program.cs
+++ MathGame/Program.cs
@@ -1,40 +1,11 @@
-﻿using MathGame.Game;
-using MathGame.Game.Randomizers;
+﻿using MathGame.Game.Randomizers;
 using MathGame.Operations;
-using MathGame.UI.Game;
-using MathGame.UI.Menu;
+using MathGame.UI;
 
-var menu = new Menu();
 var random = new Random();
-var operationFactory = new OperationFactory(new AdditionRandomizer(random), new SubtractionRandomizer(random),
-    new MultiplicationRandomizer(random), new DivisionRandomizer(random));
-var gameHistory = new GameHistory();
+var gameLoop = new GameLoop(new OperationFactory(new AdditionRandomizer(random), new SubtractionRandomizer(random),
+    new MultiplicationRandomizer(random), new DivisionRandomizer(random)));
 
-do
-{
-    MenuRenderer.Render(menu);
-    var menuChoice = MenuChoiceReader.GetChoice();
-    if (menuChoice == MenuChoiceEnum.Quit)
-    {
-        break;
-    }
-
-    if (menuChoice == MenuChoiceEnum.History)
-    {
-        HistoryRenderer.Render(gameHistory);
-        continue;
-    }
-
-    DifficultyLevelRenderer.Render();
-    var difficultyLevel = DifficultyLevelReader.GetChoice();
-
-    Console.Clear();
-    var gamePlay =
-        new GamePlay(operationFactory.Create(MenuChoiceToOperationMapper.Map(menuChoice), difficultyLevel));
-    GamePlayRenderer.Render(gamePlay);
-    var gameResult = gamePlay.GiveAnswer(AnswerReader.GetAnswer());
-    gameHistory.Add(gameResult);
-    GameResultRenderer.Render(gameResult);
-} while (true);
+gameLoop.Run();
 
 Console.Clear();
---
+++ MathGame/UI/GameLoop.cs
@@ -0,0 +1,41 @@
+using MathGame.Game;
+using MathGame.Operations;
+using MathGame.UI.Game;
+using MathGame.UI.Menu;
+
+namespace MathGame.UI;
+
+public class GameLoop(OperationFactory operationFactory)
+{
+    public void Run()
+    {
+        var gameHistory = new GameHistory();
+        var menu = new Menu.Menu();
+        do
+        {
+            MenuRenderer.Render(menu);
+            var menuChoice = MenuChoiceReader.GetChoice();
+            if (menuChoice == MenuChoiceEnum.Quit)
+            {
+                break;
+            }
+
+            if (menuChoice == MenuChoiceEnum.History)
+            {
+                HistoryRenderer.Render(gameHistory);
+                continue;
+            }
+
+            DifficultyLevelRenderer.Render();
+            var difficultyLevel = DifficultyLevelReader.GetChoice();
+
+            Console.Clear();
+            var gamePlay =
+                new GamePlay(operationFactory.Create(MenuChoiceToOperationMapper.Map(menuChoice), difficultyLevel));
+            GamePlayRenderer.Render(gamePlay);
+            var gameResult = gamePlay.GiveAnswer(AnswerReader.GetAnswer());
+            gameHistory.Add(gameResult);
+            GameResultRenderer.Render(gameResult);
+        } while (true);
+    }
+}
