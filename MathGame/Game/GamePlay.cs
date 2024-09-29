094cd09f8f592db9e444f6c38d15565cc808c8c6
Aleksander Wons <aleksander.wons@gmail.com>
2024-09-29 15:21:25 +0200

    Change primary constructor to regular constructor

--- MathGame/Game/GamePlay.cs
+++ MathGame/Game/GamePlay.cs
@@ -2,14 +2,22 @@ using MathGame.Operations;
 
 namespace MathGame.Game;
 
-public class GamePlay(IOperation operation)
+public class GamePlay
 {
-    private readonly DateTime _startTime = DateTime.Now;
+    private readonly DateTime _startTime;
     private DateTime _endTime;
+
+    public IOperation Operation { get; }
+
+    public GamePlay(IOperation operation)
+    {
+        Operation = operation;
+        _startTime = DateTime.Now;
+    }
 
     public GameResult GiveAnswer(int answer)
     {
         _endTime = DateTime.Now;
-        return new GameResult(operation, answer, _endTime - _startTime);
+        return new GameResult(Operation, answer, _endTime - _startTime);
     }
 }
