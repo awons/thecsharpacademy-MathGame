4223ab30d68b392fae52808448ecda25e0112769
Aleksander Wons <aleksander.wons@gmail.com>
2024-10-15 19:17:09 +0200

    Add tests for game result renderer

--- TestMathGame/Ui/Game/GameResultRendererTests.cs
+++ TestMathGame/Ui/Game/GameResultRendererTests.cs
@@ -1,6 +1,56 @@
+using System.Text;
+using FluentAssertions;
+using MathGame.Game;
+using MathGame.Operations.Addition;
+using MathGame.UI;
+using MathGame.UI.Game;
+using Moq;
+
 namespace TestMathGame.Ui.Game;
 
 public class GameResultRendererTests
 {
-    //TODO add tests
+    private IKeyAwaiter _keyAwaiter;
+    private StringBuilder _consoleOutput;
+
+    [SetUp]
+    public void Setup()
+    {
+        _consoleOutput = new StringBuilder();
+        var consoleOutputWriter = new StringWriter(_consoleOutput);
+        Console.SetOut(consoleOutputWriter);
+
+        var keyAwaiterMock = new Mock<IKeyAwaiter>();
+        keyAwaiterMock.Setup(k => k.Wait());
+        _keyAwaiter = keyAwaiterMock.Object;
+    }
+
+    [Test]
+    [CancelAfter(1000)]
+    public void WillRenderGameResultWhenWon()
+    {
+        var renderer = new GameResultRenderer(_keyAwaiter);
+        renderer.Render(new GameResult(new AdditionOperation(new Augend(2), new Addend(3)), 5, TimeSpan.Zero));
+
+        var output = _consoleOutput.ToString().Split(Environment.NewLine);
+
+        output[0].Should().Be("Congratulations! You won!");
+        output[1].Should().Be("It took you 0s to solve the problem");
+        output[2].Should().Be("");
+    }
+
+    [Test]
+    [CancelAfter(1000)]
+    public void WillRenderGameResultWhenLost()
+    {
+        var renderer = new GameResultRenderer(_keyAwaiter);
+        renderer.Render(new GameResult(new AdditionOperation(new Augend(2), new Addend(3)), 4, TimeSpan.Zero));
+
+        var output = _consoleOutput.ToString().Split(Environment.NewLine);
+
+        output[0].Should().Be("Sorry, you lost.");
+        output[1].Should().Be("The result of 2 + 3 is 5");
+        output[2].Should().Be("It took you 0s to solve the problem");
+        output[3].Should().Be("");
+    }
 }
